97fae740df363af56b1e0e3bcb1cbb55b4c31715
sqlmerr <sqlmerr@ya.ru>
2024-05-07 16:18:37 +0300

    💎 Pretty logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::Router;
 use std::net::SocketAddr;
+use log::info;
 
 pub mod db;
 pub mod models;
@@ -9,10 +10,12 @@ pub mod utils;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+
     let app = Router::new().merge(routes::init_routers());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("listening on {}", addr);
+    info!("listening on http://{}", addr);
 
     let server = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(server, app).await.unwrap();
